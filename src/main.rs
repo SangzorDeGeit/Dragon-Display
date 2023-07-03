2c4eb1afa16929e2b010af4a3ae9ea90b0548275
Willem Stronkhorst <willemstronkhorst@outlook.com>
2023-07-03 18:25:29 +0200

    working on config file, intermediate save

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 //Packages for configuration file
 use std::fs::File;
 use std::io::{Read, ErrorKind};
+use druid::lens::Unit;
 use druid::piet::cairo::glib::FileError;
 use toml;
 use serde::Deserialize;
@@ -8,7 +9,7 @@ use serde::Deserialize;
 //Packages for GUI
 use druid::widget::prelude::*;
 use druid::widget::{Flex, Label, Button};
-use druid::{AppLauncher, Data, UnitPoint, WidgetExt, WindowDesc};
+use druid::{commands, AppLauncher, Data, UnitPoint, WidgetExt, WindowDesc, FileDialogOptions, AppDelegate, DelegateCtx, Target, Command, Handled};
 
 
 
@@ -22,9 +23,9 @@ enum rclone {
 #[derive(Debug, Deserialize)]
 struct Configuration {
     rclone: Option<rclone>,
-    syncDirectory: Option<String>,
-    imageDirectory: String,
-    defaultScreen: i32
+    sync_directory: Option<String>,
+    image_directory: String,
+    default_screen: i32
 }
 
 #[derive(Clone, Data)]
@@ -32,15 +33,40 @@ struct Campaign {
     name: String
 }
 
-fn configGUI() {
+struct Delegate;
 
-    todo!();
+fn config_gui() -> impl Widget<u8> {
+    // let message = Label::new("Choose if you want to synchronize images via rclone. \n 
+    // Choose none if you do not want to use the synchronization feature.");
+    let select_dialog_options = FileDialogOptions::new()
+        .select_directories()
+        .name_label("Target")
+        .title("Select folder with display images")
+        .button_text("Select");
+    let mut message = Label::new("Choose the location of the images \nImages in this folder can be displayed by the program");
+    let select = Button::new("Select folder").on_click(move |ctx, data, _| 
+        {ctx.submit_command(commands::SHOW_OPEN_PANEL.with(select_dialog_options.clone()))});
+
+    Flex::column()
+        .with_child(message)
+        .with_spacer(20.0)
+        .with_child(select)
+        .align_vertical(UnitPoint::CENTER)
 }
 
-fn config() -> File {
-    File::create("config.toml");
 
-   todo!();
+fn config() -> File {
+    let config_screen = WindowDesc::new(config_gui())
+        .title("Configurations")
+        .window_size((200.0, 200.0));
+
+    AppLauncher::with_window(config_screen)
+        .delegate(Delegate)
+        .log_to_console()
+        .launch(1.to_owned())
+        .expect("Failed to start application");
+
+    File::create("config.toml").expect("Could not create file")
 }
 
 fn read_config(){
@@ -58,8 +84,11 @@ fn read_config(){
     println!("{}", contents);
 }
 
+fn start_program(data: Campaign) {
+    todo!()
+}
 
-fn choose_campaign_widget() -> impl Widget<Campaign>{
+fn choose_campaign_widget() -> impl Widget<Campaign> {
     let message = Label::new("Choose campaign:");
     let dummy_campaign = Button::new(|data: &Campaign, _env: &Env| data.name.to_string());
 
@@ -75,7 +104,7 @@ fn main() {
     //Describe main window
     let campaign_selection = WindowDesc::new(choose_campaign_widget())
         .title("Dragon-Display")
-        .window_size((400.0, 400.0));
+        .window_size((100.0, 100.0));
 
     let option: Campaign = Campaign { 
         name: "Uclia".into(),
@@ -85,4 +114,19 @@ fn main() {
         .log_to_console()
         .launch(option)
         .expect("Failed to start application");
+}
+
+impl AppDelegate<u8> for Delegate {
+    fn command(
+        &mut self,
+        ctx: &mut DelegateCtx,
+        target: Target,
+        cmd: &Command,
+        data: &mut u8,
+        env: &Env
+    ) -> Handled {
+        if let Some(file_info) = cmd.get(commands::OPEN_FILE) {
+            
+        }
+    }
 }
